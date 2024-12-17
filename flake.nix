9d699a4b819a730c783b23344300c809fb189008
Preston TW <preston_tunnell_wilson@brown.edu>
2024-12-17 00:06:24 -0500

    Add more items from brew

--- flake.nix
+++ flake.nix
@@ -37,8 +37,12 @@
           # Specifies things to pin in the flake registry and in NIX_PATH.
           pinned = { nixpkgs = toString nixpkgs; };
           paths = with pkgs; [
-            stow
             direnv
+            # helix
+            jq
+            # starship
+            stow
+            watchexec
             zellij
           ];
         };
